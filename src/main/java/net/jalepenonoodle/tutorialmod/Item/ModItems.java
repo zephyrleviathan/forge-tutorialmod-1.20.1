9012bd8aa7d2477959d9564896597bb0deb5c2de
Zephyr Leviathan <sharkoseventyseven+zl@gmail.com>
2024-12-15 11:02:52 -0800

    video 6: Metal Detector

--- src/main/java/net/jalepenonoodle/tutorialmod/Item/ModItems.java
+++ src/main/java/net/jalepenonoodle/tutorialmod/Item/ModItems.java
@@ -1,6 +1,7 @@
 package net.jalepenonoodle.tutorialmod.Item;
 
 import net.jalepenonoodle.tutorialmod.TutorialMod;
+import net.jalepenonoodle.tutorialmod.Item.custom.MetalDetectorItem;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -13,8 +14,12 @@ public class ModItems {
     
     public static final RegistryObject<Item> SAPPHIRE = ITEMS.register("sapphire", 
         () -> new Item(new Item.Properties()));
+        
     public static final RegistryObject<Item> RAW_SAPPHIRE = ITEMS.register("raw_sapphire",
         () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> METAL_DETECTOR = ITEMS.register("metal_detector",
+        () -> new MetalDetectorItem(new Item.Properties().durability(100)));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
